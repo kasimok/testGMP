57382d4724fdc207cd1c48c13eb48055b9276cf7
0x67 <evilisnj@gmail.com>
2023-09-21 13:10:34 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Sample Fat universal xcframework that works on arm64/arm64e ios/isimulator 
---
+++ testGMP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testGMP
+//
+//  Created by 0x67 on 2023-09-20.
+//
+
+import UIKit
+import Libgmp
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    let s = __mpq_struct()
+  }
+
+
+}
+
